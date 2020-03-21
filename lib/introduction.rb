98d22ca1d96febb3f8876f719fe96542ff70d844
Ashley Etemadi <ash.etemadi@gmail.com>
2020-03-21 01:21:21 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -13,8 +13,10 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-#
-
+#Define a Method With an Optional Parameter
+def introduction_with_language_optional(name, language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 
 
